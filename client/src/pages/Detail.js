3c2eed7c7905ad8a69a4347b8f4d15ff9ecb723b
samschool789@gmail.com <samschool789@gmail.com>
2022-08-10 18:57:33 -0500

    hooks

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
-function Detail() {
-  const { id } = useParams();
+const [state, dispatch] = useStoreContext();
+const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({});
+const [currentProduct, setCurrentProduct] = useState({})
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+const { products } = state;
 
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    }
-  }, [products, id]);
+useEffect(() => {
+  if (products.length) {
+    setCurrentProduct(products.find(product => product._id === id));
+  } else if (data) {
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: data.products
+    });
+  }
+}, [products, data, dispatch, id]);
 
   return (
     <>
@@ -45,6 +51,5 @@ function Detail() {
       {loading ? <img src={spinner} alt="loading" /> : null}
     </>
   );
-}
 
 export default Detail;
